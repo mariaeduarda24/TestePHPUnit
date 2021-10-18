d674cb12713b936285e2a4569ecaa6be709651f9
Evellyn Ortega <57270931+evelineeu@users.noreply.github.com>
2021-10-18 18:19:25 -0300

    comitei o formulario.php

--- formulario.php
+++ formulario.php
@@ -1,6 +1,21 @@
 <?php
 
-echo
+$n1 =  $_POST["numero1"];
+$n2 =  $_POST["numero2"];
+
+$soma = $n1 + $n2;
+
+$sub = $n1 - $n2;
+
+$mult = $n1 * $n2;
+
+$divisao = $n1 / $n2;
 
 
+echo "O resultado da soma é: $soma";
+echo "<br>O resultado da subtracao é: $sub";
+echo "<br>O resultado da divisão é: $divisao " ;
+echo "<br>O resultado da multiplicacao é: $mult";
 ?>
+
+
