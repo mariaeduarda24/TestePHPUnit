7d0fc697a90d1be059abeae4f428aba77fc39aa5
Evellyn Ortega <57270931+evelineeu@users.noreply.github.com>
2021-10-18 18:27:26 -0300

    form`s here

--- formulario.php
+++ formulario.php
@@ -1,8 +1,10 @@
 <?php
 
+//digitando os números
 $n1 =  $_POST["numero1"];
 $n2 =  $_POST["numero2"];
 
+//fazendo as operações
 $soma = $n1 + $n2;
 
 $sub = $n1 - $n2;
@@ -11,11 +13,12 @@ $mult = $n1 * $n2;
 
 $divisao = $n1 / $n2;
 
-
+// exibindo os resultados das operações 
 echo "O resultado da soma é: $soma";
 echo "<br>O resultado da subtracao é: $sub";
 echo "<br>O resultado da divisão é: $divisao " ;
 echo "<br>O resultado da multiplicacao é: $mult";
+
 ?>
 
 
