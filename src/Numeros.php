be68f0eb487caa3bbcd9c132bc3b10f2971a7a55
Evellyn Ortega <evellynjco@gmail.com>
2021-10-28 17:35:15 -0300

    commitei Numeros.php

--- src/Numeros.php
+++ src/Numeros.php
@@ -18,39 +18,10 @@ class Numeros
 	inserir( 2 ); -> [ 3, 1, 2 ]
 	*/	
 	public function inserir( $numero ){	
-
-		array_push($this->numeros, $numero);
+		array_push($this->numeros, $numero);				
 	}				
 
-	/*
-	Procura e número solicitado no vetor e o remove do vetor se o achar	
-	
-	Retorna:
-	- true se encontrou e removeu com sucesso
-	- false se não encontro o número
-	*/
-	public function remover( $numero ) {
-		$retorno;
-		if(in_array($numero,$this->numeros)==true){
-			$indice = array_search($numero, $this->numeros);
-			unset($this->numeros[$indice]);
-			$retorno = "true";
-		}
-		else{
-			$retorno = "false";
-		}
-		
-		return $retorno;
-	}
-	
-	/*
-		Retorna o menor número da lista
-	*/
-	public function menor(){
-		
-		return min($this->numeros);		
-	}
-	
+
 	/*
 		Retorna o maior número da lista
 	*/	
@@ -60,56 +31,6 @@ class Numeros
 			
 	}
 	
-	/*
-		Conta quantas vezes um número apareceu na lista
-	*/	
-	public function contar( $numero ){
-		
-       	 
-		$indice=array_search($numero, $this->numeros);
-		$total = 0;
-		for($i=0;$i<count($this->numeros);$i++){
-		  		if($this->numeros[$i]==$this->numeros[$indice])
-		  			$total++;
-		  	}
-
-		return ($total);  		
-	}
-		
-	/* 
-		Retorna a soma dos numeros armazenados na lista
-	*/
-	public function soma(){
-
-		$soma = 0;
-
-		for($i=0;$i<count($this->numeros);$i++)
-		{
-		
-			$soma=$this->numeros[$i]*$soma;
-		}
-
-
-		return $soma;		
-	}
-	
-	/*
-		Retorna o produto dos numeros armazenados na lista
-	
-	*/	
-	public function produto(){
-
-		$produto=1;
-
-		for($i=0;$i<count($this->numeros);$i++)
-		{
-		
-			$produto=$this->numeros[$i]*$produto;
-		}
-
-		return $produto;
-	}
-		
 	/* 
 		Retorna a quantidade de numeros armazenados
 	*/
