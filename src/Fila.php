b5dd3a70f93dc832d794ccf614141370711667a9
Larissa Schumacker <57271016+Larissa-22@users.noreply.github.com>
2021-10-28 15:40:12 -0300

    Adicionei Fila.php na pasta src

---
+++ src/Fila.php
@@ -0,0 +1,47 @@
+<?php
+
+class Fila
+{	
+	private $fila;
+
+	public function __construct(){	
+		$this->fila = array();			
+	}
+	
+	/*
+	Fila vazia : []
+	inserir( 1 ); -> [ 1 ]
+	inserir( 2 ); -> [ 1, 2 ]
+	inserir( 3 ); -> [ 1, 2, 3 ]
+	*/	
+	public function inserir( $dado ){
+		array_push($this->fila, $dado);				
+	}
+	
+	/*
+	Considerando a fila com [ 1, 2, 3 ], após as inserções acima:
+	remover() -> [ 2 , 3 ] e retorna 1
+	remover() -> [ 3 ] e retorna 2
+	remover() -> [  ] e retorna 3
+	*/
+	public function remover() {
+		$primeiroDado = array_shift($this->fila);
+		return $primeiroDado;
+	}
+	
+	// Funciona como o remover() mas não remove o topo da fila
+	public function topo(){
+		$primeiroDado = reset($this->fila);
+		return $primeiroDado;		
+	}
+	
+	// Retorna a quantidade de itens da fila
+	public function total_de_itens(){
+		$tamanho = count($this->fila);
+		return $tamanho;
+	}
+	
+}
+
+
+?>
